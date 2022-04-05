c936a5cebac5d046105d03055f650a267dead16c
Nico Guth <nico.guth@tu-dortmund.de>
2022-04-05 15:00:46 +0200

    Plotted the feature correlation with pair plots

--- data_investigation/feature_correlation.py
+++ data_investigation/feature_correlation.py
@@ -4,13 +4,23 @@ import sys
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+import matplotlib as mpl
 from pathlib import Path
 from tqdm.auto import tqdm
 
 # Imports from this project
 sys.path.insert(0, str(Path(__file__).parent.parent))
 from utils.input_output import load_feature_keys, load_feature_properties, load_preprocessed_data
+from utils.histograms import find_good_binning
+from utils.merge_pdfs import merge_pdfs
 from utils import paths
+
+# %%
+# Constants
+output_dir = paths.plots_dir/"feature_correlation"
+output_dir.mkdir(parents=True, exist_ok=True)
+
+output_file = paths.plots_dir/"feature_correlation.pdf"
 
 # %%
 # Read in the feature keys
@@ -27,12 +37,16 @@ print("Done reading input")
 
 # %%
 # Apply cuts to the data
-df_data_cut = df_data
+lower_quantile = 0.0001
+higher_quantile = 0.9999
+
+mask = True
 for feature in tqdm(feature_keys, desc="Apply Feature Cuts"):
     if fprops[feature]["feature_type"] == "numerical":
-        mask = fprops[feature]["quantile_0.0001"] <= df_data_cut[feature]
-        mask &= df_data_cut[feature] <= fprops[feature]["quantile_0.9999"]
-        df_data_cut = df_data_cut[mask]
+        mask &= fprops[feature][f"quantile_{lower_quantile}"] <= df_data[feature]
+        mask &= df_data[feature] <= fprops[feature][f"quantile_{higher_quantile}"]
+        
+df_data_cut = df_data[mask]
 
 print(f"Tracks before the cuts: {df_data.shape[0]}")
 print(f"Tracks after the cuts: {df_data_cut.shape[0]}")
@@ -51,7 +65,9 @@ df_corr = df_data_cut[feature_keys].corr()
 # Plot the correlation matrix
 N_features = len(feature_keys)
 plt.figure(figsize=(N_features/4,N_features/4))
-plt.title("Feature Correlation")
+plt.title(f"""Feature Correlation
+Evaluated with cuts at quantiles {lower_quantile}-{higher_quantile}
+Tracks: {df_data_cut.shape[0]} / {df_data.shape[0]}""")
 
 ax_img = plt.matshow(df_corr, vmin=-1, vmax=+1, fignum=0, cmap="seismic")
 
@@ -65,27 +81,83 @@ cax = divider.append_axes("right", size="5%", pad=0.05)
 plt.colorbar(ax_img, cax=cax)
 
 plt.tight_layout()
-plt.savefig(paths.plots_dir/"feature_correlation.pdf")
+plt.savefig(output_dir/"00_feature_correlation.pdf")
 plt.show()
 
 # %%
 # List the highest correlations
 
-temp_df = df_corr.copy()
+df_highest_corr = df_corr.copy()
 # set lower triangular matrix (with diagonal) to nan
-triu_mask = np.triu(np.ones(temp_df.shape), k=1).astype(bool)
-temp_df.where(triu_mask, np.nan, inplace=True)
-# set all abs values lower than 0.8 to nan
-temp_df.where(np.abs(temp_df)>0.8, np.nan, inplace=True)
+triu_mask = np.triu(np.ones(df_highest_corr.shape), k=1).astype(bool)
+df_highest_corr.where(triu_mask, np.nan, inplace=True)
+# set all abs values lower than 0.7 to nan
+df_highest_corr.where(np.abs(df_highest_corr)>0.7, np.nan, inplace=True)
 # list of all pairs that have a high correlation
-temp_df.dropna(how="all", inplace=True)
-temp_df = temp_df.stack().reset_index()
+df_highest_corr.dropna(how="all", inplace=True)
+df_highest_corr = df_highest_corr.stack().reset_index()
 # print and save the list
-temp_df.rename(columns={"level_0":"f0", "level_1":"f1", 0:"correlation"}, inplace=True)
-temp_df.sort_values(by="correlation", ascending=False, inplace=True)
-temp_df = temp_df[["correlation", "f0", "f1"]]
-print(temp_df)
-temp_df.to_csv(paths.plots_dir/"feature_correlation.csv", float_format="%.4f", index=False)
+df_highest_corr.rename(columns={"level_0":"f0", "level_1":"f1", 0:"correlation"}, inplace=True)
+df_highest_corr.sort_values(by="correlation", ascending=False, key=abs, inplace=True)
+df_highest_corr = df_highest_corr[["correlation", "f0", "f1"]]
+df_highest_corr.reset_index(drop=True, inplace=True)
+print(df_highest_corr)
+df_highest_corr.to_csv(output_dir/"00_feature_correlation.csv", float_format="%.4f", index=False)
 
 # %%
-# TODO make scatter plots (or heatmap plots) for the highest correlation pairs
+# Plot the feature pairs with the highest correlation (scatterplot and hist2d plot)
+
+for i, (corr, f0, f1) in tqdm(df_highest_corr.iterrows(), total=df_highest_corr.shape[0], desc="Pair Plots"):
+    
+    bin_edges_0, bin_centers_0, is_logx_0 = find_good_binning(fprops[f0], 
+                                                      n_bins_max=100, 
+                                                      lower_quantile=0.0001, 
+                                                      higher_quantile=0.9999, 
+                                                      allow_logx=True)
+    
+    bin_edges_1, bin_centers_1, is_logx_1 = find_good_binning(fprops[f1], 
+                                                      n_bins_max=100, 
+                                                      lower_quantile=0.0001, 
+                                                      higher_quantile=0.9999, 
+                                                      allow_logx=True)
+    
+    data0 = df_data[f0].to_numpy()
+    data1 = df_data[f1].to_numpy()
+    
+    fig, (ax0, ax1) = plt.subplots(1,2, figsize=(12,6), sharex=True, sharey=True)
+    fig.suptitle(f"Pair Plot ('{f0}' vs '{f1}')\ncorrelation: {corr:.5f}")
+    
+    ax0.set_title("Scatter Plot")
+    ax0.scatter(data0, data1, s=1.0, rasterized=True)
+    ax0.set_xlabel(f0)
+    ax0.set_ylabel(f1)
+    
+    ax1.set_title("2D Histogram")
+    hist = ax1.hist2d(data0, data1, 
+               bins=[bin_edges_0, bin_edges_1], 
+               density=False,
+               norm=mpl.colors.LogNorm(),
+               cmap="inferno",
+               rasterized=True)
+    ax1.set_xlabel(f0)
+    ax1.set_ylabel(f1)
+    ax1.set_facecolor('black')
+    
+    if is_logx_0:
+        ax1.set_xscale("log")
+    if is_logx_1:
+        ax1.set_yscale("log")
+    
+    cbar = plt.colorbar(hist[3])
+    cbar.ax.set_ylabel("Counts")
+    
+    plt.tight_layout()
+    plt.savefig(output_dir/f"01_pair_plot_{i}_{f0}_{f1}.pdf")
+    #plt.show()
+    plt.close()
+    
+# %%
+# Merge all plots
+merge_pdfs(output_dir, output_file)
+
+# %%
